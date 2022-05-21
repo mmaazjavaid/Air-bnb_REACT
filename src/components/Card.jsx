8699a691f225b5907cf9e143139c0fcd41955edb
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-05-21 11:39:20 +0500

    static card done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,13 +6,13 @@ const Card = () => {
         <div className="card">
         <img src={person} className="card--image" />
         <div className="card--stats">
-            <img src={star} />
+            <img src={star} className="card--star" />
             <span>5.0</span>
-            <span>(6) • </span>
-            <span>USA</span>
+            <span className="gray">(6) • </span>
+            <span className="gray">USA</span>
         </div>
-        <p></p>
-        <p></p>
+        <p>Life Lessons with Katie Zaferes</p>
+        <p><span className="bold">From $136</span> / person</p>
     </div>
     );
 };
